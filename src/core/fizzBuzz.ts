e95dad827290f609f5e3662828cec9113495c83a
RFigueroa <48028483+FRobertoFigueroaC@users.noreply.github.com>
2024-09-14 12:09:02 -0600

    fizz buzz basic cases

--- src/core/fizzBuzz.ts
+++ src/core/fizzBuzz.ts
@@ -1,6 +1,13 @@
 export function fizzBuzz (number:number){
-    if (number === 3) {
-        return 'fizz';
+    if ( number === 15 ) {
+        return 'fizzbuzz';
     }
+    if (number % 3 === 0) {
+        return 'fizz';
+    } else if(number === 5){
+        return 'buzz'
+    }
+
+    
     return number.toString();
 }
--- src/tests/fizzBuzz.test.ts
+++ src/tests/fizzBuzz.test.ts
@@ -9,4 +9,13 @@ describe('FizzBuzz', () => {
     it('returns fizz as string for number three', () => {
         expect(fizzBuzz(3)).toBe('fizz')
     });
+    it('returns buzz as string for number five', () => {
+        expect( fizzBuzz( 5 ) ).toBe('buzz')
+    });
+    it('returns fizzbuzz as string for number fifteen', () => {
+        expect( fizzBuzz( 15 ) ).toBe('fizzbuzz')
+    });
+    it('returns fizz for any number divisible by three', () => {
+        expect( fizzBuzz( 9 ) ).toBe('fizz')
+    });
 });
