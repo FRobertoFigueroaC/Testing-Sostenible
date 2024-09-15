1424a1154ff9dd16a3f5137e19a547d12009407a
RFigueroa <48028483+FRobertoFigueroaC@users.noreply.github.com>
2024-09-14 20:23:36 -0600

    all camel case test

--- src/core/camelCase.ts
+++ src/core/camelCase.ts
@@ -1,6 +1,7 @@
 export function toCamelCase(text:string){
     const words = text.split( /[ _-]/ );
 
-    return words.map( word => word = word.charAt( 0 ).toUpperCase() + word.substr( 1 ))
-    .join('');
+    return words.map( 
+        word => word = word.charAt( 0 ).toUpperCase() + word.substr( 1 )
+    ).join('');
 }
--- src/tests/camelCase.test.ts
+++ src/tests/camelCase.test.ts
@@ -1,17 +1,17 @@
 import { toCamelCase } from '../core/camelCase';
 describe('Camel case coverter (Pasacal)', () => {
-    // it('allows empty text ', () => {
-    //     expect(toCamelCase('')).toBe('')
-    // });
-    // it('allows capitalized word ', () => {
-    //     expect(toCamelCase('Foo')).toBe('Foo')
-    // });
-    // it('joins the capitalized words that are separated by spaces', () => {
-    //     expect(toCamelCase('Foo Bar')).toBe('FooBar')
-    // });
-    // it('joins the capitalized words that are separated by hypens', () => {
-    //     expect(toCamelCase('Foo_Bar-Foo')).toBe('FooBarFoo')
-    // });
+    it('allows empty text ', () => {
+        expect(toCamelCase('')).toBe('')
+    });
+    it('allows capitalized word ', () => {
+        expect(toCamelCase('Foo')).toBe('Foo')
+    });
+    it('joins the capitalized words that are separated by spaces', () => {
+        expect(toCamelCase('Foo Bar')).toBe('FooBar')
+    });
+    it('joins the capitalized words that are separated by hypens', () => {
+        expect(toCamelCase('Foo_Bar-Foo')).toBe('FooBarFoo')
+    });
     it('Capitalize the first character of a word', () => {
         expect(toCamelCase('foo')).toBe('Foo')
     });
